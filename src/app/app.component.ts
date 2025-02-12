086e2e19a293e4e997c72e36ac1777acdf84d164
uerbzr <nigel.sibbert@gmail.com>
2025-02-12 19:15:24 +0000

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-first';
+  beers = ['Corona', 'Heineken', 'Stella'];
+  counter: number = 0;
+  votes: string[] = [];
+  Increment() {
+    this.counter++;
+  }
+  Decrement() {
+    this.counter--;
+  }
+  Vote(beer: string) {
+    console.log(`Voted for ${beer}`);
+    this.votes.push(beer);
+  }
+  CountVotes(beer: string) {
+    return this.votes.filter((vote) => vote === beer).length;
+  }
 }
